a06c1ed57d35c6b2a1b63027a6fbf35523435d5f
fengluoye2012 <1522988552@qq.com>
2019-06-25 11:12:36 +0800

    模版

---
+++ cmake-build-debug/CMakeFiles/CTest.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mac/Desktop/C:C++/cdemo/Freshman.cpp" "/Users/mac/Desktop/C:C++/cdemo/cmake-build-debug/CMakeFiles/CTest.dir/Freshman.cpp.o"
+  "/Users/mac/Desktop/C:C++/cdemo/HighStudent.cpp" "/Users/mac/Desktop/C:C++/cdemo/cmake-build-debug/CMakeFiles/CTest.dir/HighStudent.cpp.o"
+  "/Users/mac/Desktop/C:C++/cdemo/Student.cpp" "/Users/mac/Desktop/C:C++/cdemo/cmake-build-debug/CMakeFiles/CTest.dir/Student.cpp.o"
+  "/Users/mac/Desktop/C:C++/cdemo/StudentManagerHelper.cpp" "/Users/mac/Desktop/C:C++/cdemo/cmake-build-debug/CMakeFiles/CTest.dir/StudentManagerHelper.cpp.o"
+  "/Users/mac/Desktop/C:C++/cdemo/TemplateTest.cpp" "/Users/mac/Desktop/C:C++/cdemo/cmake-build-debug/CMakeFiles/CTest.dir/TemplateTest.cpp.o"
+  "/Users/mac/Desktop/C:C++/cdemo/main.cpp" "/Users/mac/Desktop/C:C++/cdemo/cmake-build-debug/CMakeFiles/CTest.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CTest.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CTest.dir/main.cpp.o"
+  "CMakeFiles/CTest.dir/Student.cpp.o"
+  "CMakeFiles/CTest.dir/HighStudent.cpp.o"
+  "CMakeFiles/CTest.dir/Freshman.cpp.o"
+  "CMakeFiles/CTest.dir/StudentManagerHelper.cpp.o"
+  "CMakeFiles/CTest.dir/TemplateTest.cpp.o"
+  "CTest.pdb"
+  "CTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
